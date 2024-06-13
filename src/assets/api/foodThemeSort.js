d1f27903000899ec2aee6f153c238284c1c3c140
Wooyoulim <168395738+Wooyoulim@users.noreply.github.com>
2024-06-13 09:55:57 +0900

    Update foodThemeSort.js

--- src/assets/api/foodThemeSort.js
+++ src/assets/api/foodThemeSort.js
@@ -2,17 +2,17 @@ const foodThemaSort = [
     {
         id: 1,
         thema: 'summer',
-        imgurl: './public/images/food/foodtheme01.jpg',
+        imgurl: './images/food/foodtheme01.jpg',
     },
     {
         id: 2,
         thema: 'brunch',
-        imgurl: './public/images/food/foodtheme02.jpg',
+        imgurl: './images/food/foodtheme02.jpg',
     },
     {
         id: 3,
         thema: 'wholecake',
-        imgurl: './public/images/food/foodtheme03.jpg',
+        imgurl: './images/food/foodtheme03.jpg',
     },
 ];
 
